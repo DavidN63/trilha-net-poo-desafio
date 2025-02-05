7535c9e2c6213686a906a6fc76891d825049058e
DavidN63 <120107802+DavidN63@users.noreply.github.com>
2025-02-04 22:14:41 -0300

    Update Iphone.cs

--- Models/Iphone.cs
+++ Models/Iphone.cs
@@ -1,8 +1,13 @@
-namespace DesafioPOO.Models
-{
-    // TODO: Herdar da classe "Smartphone"
-    public class Iphone
-    {
-        // TODO: Sobrescrever o método "InstalarAplicativo"
-    }
-}
+using DesafioPOO.Models;
+
+Console.WriteLine("Smartphone Nókia:");
+Smartphone nokia = new Nokia(numero: "123456", modelo: "modelo 1", imei: "1111111", memoria: 64);
+nokia.Ligar(); // Corrigido para o método Ligar
+nokia.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone:");
+Smartphone iphone = new Iphone(numero: "4987", modelo: "modelo 2", imei: "22222222", memoria: 128);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Telegram");
